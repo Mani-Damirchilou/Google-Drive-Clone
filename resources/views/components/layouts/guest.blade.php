3850b95636e873bc404598021b4d29b2f2f67dac
mani-damirchilou <manidamirchilou@gmail.com>
2024-12-28 10:39:36 -0800

    Add theme switch to guest layout

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -1,3 +1,4 @@
-<main class="min-h-screen flex flex-col justify-center items-center bg-base-300 gap-10">
+<main class="min-h-screen flex flex-col justify-center items-center bg-base-300 gap-10 relative">
+    <x-ui.theme-switch class="absolute top-10 right-10 btn btn-circle bg-base-100"/>
     {{$slot}}
 </main>
