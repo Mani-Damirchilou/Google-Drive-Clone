912db8abed22d3f11abaa7c48b2ad14911d39a59
mani-damirchilou <manidamirchilou@gmail.com>
2024-12-28 07:04:28 -0800

    Add azarmehr font

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Azarmehr', ...defaultTheme.fontFamily.sans],
             },
         },
     },
